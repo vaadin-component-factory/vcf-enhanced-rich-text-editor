3cff6f6f5ecc333510a75333566d41098e73afdd
Nii Yeboah <nii@vaadin.com>
2020-11-05 12:20:15 +0200

    Update src version: 1.3.5 -> 1.3.6

--- src/vcf-enhanced-rich-text-editor.js
+++ src/vcf-enhanced-rich-text-editor.js
@@ -313,7 +313,7 @@ Inline.order.push(PlaceholderBlot.blotName, ReadOnlyBlot.blotName, LinePartBlot.
     }
 
     static get version() {
-      return '1.3.5';
+      return '1.3.6';
     }
 
     static get properties() {
