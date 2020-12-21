a4bbf86d5965c9054adc89566d764595233b4ba5
Nii Yeboah <nii@vaadin.com>
2020-12-21 15:22:39 +0200

    v1.3.14

--- src/vcf-enhanced-rich-text-editor.js
+++ src/vcf-enhanced-rich-text-editor.js
@@ -322,7 +322,7 @@ Inline.order.push(PlaceholderBlot.blotName, ReadOnlyBlot.blotName, LinePartBlot.
     }
 
     static get version() {
-      return '1.3.12';
+      return '1.3.14';
     }
 
     static get properties() {
