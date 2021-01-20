1f652d3a5cfe36da0014d95e74f03fb37e450671
Nii Yeboah <nii@vaadin.com>
2021-01-20 19:40:32 +0200

    Add helper methods Add toolbar slot name

--- src/vcf-enhanced-rich-text-editor.js
+++ src/vcf-enhanced-rich-text-editor.js
@@ -275,7 +275,7 @@ Inline.order.push(PlaceholderBlot.blotName, ReadOnlyBlot.blotName, LinePartBlot.
             </span>
 
             <span part="toolbar-group toolbar-group-custom" style="display: [[_buttonGroupDisplay(toolbarButtons, 'custom')]];">
-              <slot on-slot-change="_onSlotChange"></slot>
+              <slot name="toolbar" on-slot-change="_onToolbarSlotChange"></slot>
             </span>
 
             <input id="fileInput" type="file" accept="image/png, image/gif, image/jpeg, image/bmp, image/x-icon" on-change="_uploadImage" />
@@ -774,7 +774,7 @@ Inline.order.push(PlaceholderBlot.blotName, ReadOnlyBlot.blotName, LinePartBlot.
       this._ready = true;
     }
 
-    _onSlotChange() {
+    _onToolbarSlotChange() {
       this._setCustomButtons();
     }
 
@@ -791,8 +791,6 @@ Inline.order.push(PlaceholderBlot.blotName, ReadOnlyBlot.blotName, LinePartBlot.
       const buttons = this._customButtons;
       TOOLBAR_BUTTON_GROUPS.custom = [];
       buttons.forEach((btn, i) => {
-        const icon = btn.querySelector('iron-icon');
-        if (icon) icon.style.height = '20px';
         btn.setAttribute('part', `toolbar-button toolbar-button-custom-${i}`);
         TOOLBAR_BUTTON_GROUPS.custom.push(btn.innerText);
       });
@@ -807,17 +805,52 @@ Inline.order.push(PlaceholderBlot.blotName, ReadOnlyBlot.blotName, LinePartBlot.
      */
     addCustomButton(label, tooltip, icon = '', clickListener, keyboardShortcut) {
       const btn = document.createElement('vaadin-button');
-      btn.innerText = label;
-      btn.title = tooltip;
-      if (icon) {
-        const iconEl = document.createElement('iron-icon');
+      btn.setAttribute('slot', 'toolbar');
+      this.setCustomButtonLabel(label, btn);
+      this.setCustomButtonTooltip(tooltip, btn);
+      this.setCustomButtonIcon(icon, btn);
+      this.setCustomButtonClickListener(clickListener, btn);
+      this.setCustomButtonKeyboardShortcut(keyboardShortcut, btn);
+      this.appendChild(btn);
+      this._setCustomButtons();
+    }
+
+    setCustomButtonLabel(label, btn) {
+      if (btn && label) btn.innerText = label;
+    }
+
+    setCustomButtonTooltip(tooltip, btn) {
+      if (btn && tooltip) btn.title = tooltip;
+    }
+
+    setCustomButtonIcon(icon, btn, suffix = false) {
+      if (btn && icon) {
+        let iconEl = btn.querySelector('iron-icon');
+        if (!iconEl) {
+          iconEl = document.createElement('iron-icon');
+          btn.appendChild(iconEl);
+        }
+        if (btn.tagName.toLowerCase() !== 'vaadin-button') {
+          const vaadinBtn = document.createElement('vaadin-button');
+          vaadinBtn.setAttribute('title', btn.getAttribute('title'));
+          vaadinBtn.setAttribute('part', btn.getAttribute('part'));
+          vaadinBtn.appendChild(iconEl);
+          vaadinBtn.innerText = btn.innerText;
+          btn.parentElement.replaceChild(vaadinBtn, btn);
+          btn = vaadinBtn;
+        }
+        if (!btn.innerText) btn.setAttribute('theme', 'icon');
+        else iconEl.setAttribute('slot', suffix ? 'suffix' : 'prefix');
         iconEl.setAttribute('icon', icon);
-        if (!label) btn.setAttribute('theme', 'icon');
-        else iconEl.setAttribute('slot', 'prefix');
-        btn.appendChild(iconEl);
       }
-      if (clickListener) btn.addEventListener('click', e => clickListener(e));
-      if (keyboardShortcut) {
+    }
+
+    setCustomButtonClickListener(clickListener, btn) {
+      if (btn && clickListener) btn.addEventListener('click', e => clickListener(e));
+    }
+
+    setCustomButtonKeyboardShortcut(keyboardShortcut, btn) {
+      if (btn && keyboardShortcut) {
         const keyboard = this._editor.getModule('keyboard');
         const bindings = keyboard.bindings[keyboardShortcut.key] || [];
         keyboard.bindings[keyboardShortcut.key] = [
@@ -831,14 +864,11 @@ Inline.order.push(PlaceholderBlot.blotName, ReadOnlyBlot.blotName, LinePartBlot.
           ...bindings
         ];
       }
-      this.appendChild(btn);
-      this._setCustomButtons();
     }
 
     _prepareToolbar() {
       const clean = Quill.imports['modules/toolbar'].DEFAULTS.handlers.clean;
       const self = this;
-
       const toolbar = {
         container: this.shadowRoot.querySelector('[part="toolbar"]'),
         handlers: {
@@ -1346,7 +1376,7 @@ Inline.order.push(PlaceholderBlot.blotName, ReadOnlyBlot.blotName, LinePartBlot.
     }
 
     get _customButtons() {
-      return Array.from(this.querySelectorAll('button, vaadin-button, [part="custom-button"]'));
+      return Array.from(this.querySelectorAll('button, vaadin-button, [part="custom-button"]')).filter(el => el.getAttribute('slot') === 'toolbar');
     }
 
     get _toolbarButtons() {
